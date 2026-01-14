e6439f73219022e3e70412a30620192ce4b6514c
Asher Green <76627281+ashergreen82@users.noreply.github.com>
2026-01-14 18:06:56 -0500

    Updated the Vite.config file to remove a hardcoded port element

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // Allow external connections
-    port: 5173,
+    //port: 5173,
     cors: false, // Disable built-in CORS handling
     proxy: {
       '/auth-api': {
